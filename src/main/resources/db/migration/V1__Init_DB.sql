ad081ce8834a63f18b075101dc45f437b391bf2b
nezhov <nikita.ejov2012@yandex.ru>
2021-10-13 21:49:01 +0300

    Changed DB migration script because of jpa validate conflict (different types of "quantity")

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `socks_by_type` (
-                                 `type_id` varchar(48) NOT NULL,
-                                 `color` varchar(45) DEFAULT NULL,
+                                 `type_id` varchar(255) NOT NULL,
+                                 `color` varchar(255) DEFAULT NULL,
                                  `cotton_part` int DEFAULT NULL,
-                                 `quantity` varchar(45) DEFAULT NULL,
+                                 `quantity` int DEFAULT NULL,
                                  PRIMARY KEY (`type_id`),
                                  UNIQUE KEY `type_id_UNIQUE` (`type_id`),
                                  KEY `color_key_idx` (`color`)
